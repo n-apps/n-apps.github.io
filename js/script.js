ba5a3dbda7f9980f24e64e8da4819ce80a47e67f
n-apps <roman.novodvorskiy@gmail.com>
2021-11-23 20:40:58 +0200

    Playing with fonts

---
+++ js/script.js
@@ -0,0 +1,9 @@
+Array.prototype.forEach.call(document.getElementsByTagName('a'), function(link) {
+  link.target = "_blank"
+})
+
+dayjs.locale('ru')
+dayjs.extend(window.dayjs_plugin_relativeTime)
+const date = dayjs('2021-11-23');
+
+document.getElementById("label-ago").innerHTML = date.fromNow();
