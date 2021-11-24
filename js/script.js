da54490c75e73a145b4c354ebb6b6c48973a9362
n-apps <roman.novodvorskiy@gmail.com>
2021-11-24 22:00:36 +0200

    - Update reset.css - Add hover effects

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ Array.prototype.forEach.call(document.getElementsByTagName('a'), function(link) 
 
 dayjs.locale('ru')
 dayjs.extend(window.dayjs_plugin_relativeTime)
-const date = dayjs('2021-11-23');
+const date = dayjs('2021-11-24');
 
 document.getElementById("label-ago").innerHTML = date.fromNow();
