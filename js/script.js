01d5741f30104c601f6426cbdcbfcaf300fef29b
n-apps <roman.novodvorskiy@gmail.com>
2022-01-18 15:42:39 +0200

    Update year

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ Array.prototype.forEach.call(document.getElementsByTagName('a'), function(link) 
 
 dayjs.locale('ru')
 dayjs.extend(window.dayjs_plugin_relativeTime)
-const date = dayjs('2021-12-07');
+const date = dayjs('2022-18-01');
 
 document.getElementById("label-ago").innerHTML = date.fromNow();
