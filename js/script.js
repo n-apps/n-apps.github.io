076a0d40bf63fed1d4f626341f6f69365a34746e
n-apps <roman.novodvorskiy@gmail.com>
2022-02-15 11:02:32 +0200

    Get rid of Google Analytics in favor of goatcounter

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ Array.prototype.forEach.call(document.getElementsByTagName('a'), function(link) 
 
 dayjs.locale('ru')
 dayjs.extend(window.dayjs_plugin_relativeTime)
-const date = dayjs('2022-01-18');
+const date = dayjs('2022-02-15');
 
 document.getElementById("label-ago").innerHTML = date.fromNow();
