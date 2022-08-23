ab80c55845608ab8c0f9618831e6a9ca32f05517
Roman S <roman.novodvorskiy@gmail.com>
2022-08-23 19:29:27 +0300

    Bye-bye russian. Hello Eglish!

--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@ Array.prototype.forEach.call(document.getElementsByTagName('a'), function(link) 
   link.target = "_blank"
 })
 
-dayjs.locale('ru')
 dayjs.extend(window.dayjs_plugin_relativeTime)
-const date = dayjs('2022-06-29');
+const date = dayjs('2022-08-23 19:21');
 
 document.getElementById("label-ago").innerHTML = date.fromNow();
